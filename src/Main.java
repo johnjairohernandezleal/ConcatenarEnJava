5823ea713256827d1ee82622963276797bb34395
unknown <john_0112@hotmail.com>
2019-08-13 00:31:21 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        var usuario ="juan";
+        var saludar ="Hola";
+        System.out.println(saludar + "" + usuario);
+
+        // saludos, mi nombre es Juan
+        System.out.println("Saludos, mi nombre es"+ " " + usuario);
+
+        System.out.println("saludar = " + saludar);
+    }
+}
